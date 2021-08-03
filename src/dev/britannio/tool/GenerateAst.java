9a7d3a7af84d2dbcf76fcf8dd098bab808d441bc
Britannio Jarrett <britanniojarrett@gmail.com>
2021-08-03 23:18:42 +0100

    Implement a generator for expressions. Implement a pretty printer for expressions.

---
+++ src/dev/britannio/tool/GenerateAst.java
@@ -0,0 +1,93 @@
+package dev.britannio.tool;
+
+import java.util.List;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class GenerateAst {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList("Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression", "Literal  : Object value", "Unary    : Token operator, Expr right"));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        var path = outputDir + "/" + baseName + ".java";
+        var writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package dev.britannio.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println("import dev.britannio.lox.Token;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        // The AST class
+        for (var type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.print("}");
+        writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (var type : types) {
+            var typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+
+        }
+        writer.println(" }");
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
+
+        // Constructor
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+
+        for (var field : fields) {
+            var name = field.split(" ")[1];
+            writer.println("    this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Visitor pattern
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
+
+        // Fields
+        writer.println();
+        for (var field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+
+    }
+
+}
